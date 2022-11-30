eb95b6c23eb123d09aa7267cabd9c09078e6c28a
Kellsier <114060208+KeLL-siL@users.noreply.github.com>
2022-11-30 16:15:56 +0800

    created list.py used as classwork

---
+++ .github/assets/List.py
@@ -0,0 +1,12 @@
+#this will be a list in Python
+name = ["Harry", "Ron", "Hermione"]
+#How is print the entire list
+print(names)
+#How to print the first item in the list
+print(name[0])
+#How to add a new name to the list: 
+name.append("Draco")
+#How to sort the list
+name.sort()
+#how to print the updated list
+print(names)
