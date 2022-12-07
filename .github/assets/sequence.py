fff4fdf3cf7edd2687bed5611ad9b41cb072a9a8
Kellsier <114060208+KeLL-siL@users.noreply.github.com>
2022-12-07 16:15:40 +0800

    Changes done in class

--- .github/assets/sequence.py
+++ .github/assets/sequence.py
@@ -1,6 +1,9 @@
 name = "Harry"
 # this is how you comment in python
 # Let's see what the second line looks like
+# Another line of Comment to remind yoursilf of something
+# Or to tell another programmer what you're doing on this line
 print (name[0])
 print (name[1])
 print (name[2])
+# What you want to build next
