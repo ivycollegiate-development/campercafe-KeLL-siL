555f392b7c70530e0bbfe959e29d47f0e1aa5fa0
Kellsier <114060208+KeLL-siL@users.noreply.github.com>
2022-12-09 15:38:22 +0800

    Created Set.py

---
+++ .github/assets/Set.py
@@ -0,0 +1,15 @@
+#This is how you create an empty set
+
+S = set()
+
+#Add this is how you add elements to the set:
+
+S.add(1)
+S.add(2)
+S.add(3)
+S.add(4)
+S.add(1)
+
+#This is how you print and view the contents of the set that you have made
+
+print(S)
