b400b390d7b5367f60606006b0addef7d671d737
Kellsier <114060208+KeLL-siL@users.noreply.github.com>
2022-12-12 15:47:50 +0800

    Dictionary.py

---
+++ .github/assets/dict.py
@@ -0,0 +1,5 @@
+#This is how you define a dictioonary file in python
+houses = ("Harry": "Gryffindor", "Draco": "Slytherin")
+
+#This is how you print out the dict in the console
+print(houses["Harry"])
